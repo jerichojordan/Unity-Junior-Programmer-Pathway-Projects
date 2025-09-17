c6dc3e660930e3324e85de566ce24a0e433b4d1e
Snowbits <jericho.jordan.ong@gmail.com>
2025-09-17 22:49:20 +0200

    import prototype2

---
+++ Assets/Scripts/Challenge 2/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Challenge2
+{
+    public class PlayerControllerX : MonoBehaviour
+    {
+        public GameObject dogPrefab;
+        private float currentTime;
+        private float spawnDelay = 1.0f;
+
+        // Update is called once per frame
+        void Update()
+        {
+            // On spacebar press, send dog
+            if (Input.GetKeyDown(KeyCode.Space) && Time.time > currentTime + spawnDelay)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                currentTime = Time.time;
+            }
+        }
+    }
+
+}
--- Assets/Scripts/Prototoype 1/PlayerController.cs
+++ Assets/Scripts/Prototoype 1/PlayerController.cs
@@ -1,21 +1,24 @@
 using UnityEngine;
-
-public class PlayerController : MonoBehaviour
+namespace Prototype1
 {
-    //
-    [SerializeField]private float speed = 20.0f;
-    [SerializeField]private float turnSpeed = 45.0f;
-    private float horizontalInput;
-    private float forwardInput;
-    
-    // Update is called once per frame
-    void FixedUpdate()
+
+    public class PlayerController : MonoBehaviour
     {
-        horizontalInput = Input.GetAxis("Horizontal"); 
-        forwardInput = Input.GetAxis("Vertical"); 
-        //Move the vehicle forward based on vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        //Rotates the car based on horizontal input
-        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+        //
+        [SerializeField] private float speed = 20.0f;
+        [SerializeField] private float turnSpeed = 45.0f;
+        private float horizontalInput;
+        private float forwardInput;
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            horizontalInput = Input.GetAxis("Horizontal");
+            forwardInput = Input.GetAxis("Vertical");
+            //Move the vehicle forward based on vertical input
+            transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+            //Rotates the car based on horizontal input
+            transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+        }
     }
 }
