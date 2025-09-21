c23570a8dff9bc6dd5f6107d76aa8d314dada255
Snowbits <jericho.jordan.ong@gmail.com>
2025-09-21 20:59:03 +0200

    change menuUI getting overlays using children

--- Assets/Scripts/MainMenu/MenuUIManager.cs
+++ Assets/Scripts/MainMenu/MenuUIManager.cs
@@ -5,7 +5,18 @@ using System.Collections.Generic;
 public class MenuUIManager : MonoBehaviour
 {
     [SerializeField] private TMP_Dropdown dropdown;
-    [SerializeField] private GameObject[] overlayGameobjects;
+    [SerializeField] private Transform overlayParent;
+    public GameObject[] overlayGameobjects;
+    void Start()
+    {
+        overlayGameobjects = new GameObject[overlayParent.childCount];
+
+        for (int i = 0; i < overlayParent.childCount; i++)
+        {
+            overlayGameobjects[i] = overlayParent.transform.GetChild(i).gameObject;
+        }
+
+    }
     public void HandleOpenProject()
     {
         overlayGameobjects[dropdown.value].SetActive(true);
